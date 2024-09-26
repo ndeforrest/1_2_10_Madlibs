1b6a56bb42e2ba6899b0c286ed7c8de05d010605
Nolan <nolan.deforrest@student.tamdistrict.org>
2024-09-26 09:57:37 -0700

    add a new line
    Add a new line to Madlibs.java. Totally unnecessary.
    More details here.

--- Madlibs.java
+++ Madlibs.java
@@ -4,6 +4,7 @@ public class Madlibs {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
+        
         String madlib = "I went to the animal <noun>, the <plural noun> and the <number> beasts were there.";
         //madlib = "Once upon a time in a <adjective> land, a <noun> <adjective> grew.";
         //madlib = "<name> was very <adjective> today, so they went to <place>";
