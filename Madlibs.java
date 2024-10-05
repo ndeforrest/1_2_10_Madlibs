8d073a0383b2c8bca6e6d4857efcf346a386bee8
Nolan <nolan.deforrest@student.tamdistrict.org>
2024-10-04 19:52:47 -0700

    debug index number error Madlibs.java

--- Madlibs.java
+++ Madlibs.java
@@ -11,19 +11,23 @@ public class Madlibs {
         int indexLeft1 = madlibFull.indexOf("<");
         int indexRight1 = madlibFull.indexOf(">");
         String part1 = madlibFull.substring(0, indexLeft1);
-        String partBad1 = madlibFull.substring(indexLeft1);
-        int indexLeft2 = partBad1.indexOf("<");
-        int indexRight2 = partBad1.indexOf(">");
-        String part2 = madlibFull.substring(indexRight1, indexLeft2);
-        String partBad2 = madlibFull.substring(indexRight2);
-        int indexLeft3 = partBad2.indexOf("<");
-        int indexRight3 = partBad2.indexOf(">");
-        String part3 = madlibFull.substring(indexRight2, indexLeft3);
-        String part4 = madlibFull.substring(indexRight3);
+        String partExtra1 = madlibFull.substring(indexRight1+1);
+        String partReference1 = madlibFull.substring(0,(indexRight1 + 1));
 
-        String word1 = madlibFull.substring(indexLeft1, (indexRight1-1));
-        String word2 = madlibFull.substring(indexLeft2, (indexRight2-1));
-        String word3 = madlibFull.substring(indexLeft3, (indexRight3-1));
+        int indexLeft2 = partExtra1.indexOf("<") + partReference1.length();
+        int indexRight2 = partExtra1.indexOf(">") + partReference1.length();
+        String part2 = madlibFull.substring(indexRight1+1, indexLeft2);
+        String partExtra2 = madlibFull.substring(indexRight2+1);
+        String partReference2 = madlibFull.substring(0, (indexRight2 + 1));
+
+        int indexLeft3 = partExtra2.indexOf("<") + partReference2.length();
+        int indexRight3 = partExtra2.indexOf(">") + partReference2.length();
+        String part3 = madlibFull.substring(indexRight2+1, indexLeft3);
+        String part4 = madlibFull.substring(indexRight3+1);
+
+        String word1 = madlibFull.substring(indexLeft1+1, indexRight1);
+        String word2 = madlibFull.substring(indexLeft2+1, indexRight2);
+        String word3 = madlibFull.substring(indexLeft3+1, indexRight3);
 
         System.out.println("Here's a MadLib for you!");
         System.out.println("Please give me a " + word1);
